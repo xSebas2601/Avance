3a206928cf1e18fcccebd009d0a34d3ca4f57b2e
Sebas <sebastiananyosac@gmail.com>
2020-09-23 17:41:42 -0500

    hasta el dia de hoy

---
+++ semana3/dia5/alg2.js
@@ -0,0 +1,8 @@
+let peso = +prompt("Ingrese la cantidad de pesos")
+const CORRENT_MEX_PRICE_IN_DOLLLAR = 0.0473005;
+alert(`Se usará de precio actual el valor ${CORRENT_MEX_PRICE_IN_DOLLLAR}`)
+
+let dolares = peso * CORRENT_MEX_PRICE_IN_DOLLLAR;
+
+console.log(dolares);
+console.log("Hola");
